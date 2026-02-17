d82cfcac1ed5036ca707d1a55938c9b55fe471a7
pkititsa <pkititsa200@gmail.com>
2026-02-17 16:13:38 +0300

    Added base test and tests for book api

---
+++ src/test/java/com/kit/killrilltesty/tests/BaseTest.java
@@ -0,0 +1,22 @@
+package com.kit.killrilltesty.tests;
+
+import com.kit.killrilltesty.api.specs.RequestSpec;
+import com.kit.killrilltesty.api.specs.ResponseSpec;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public abstract class BaseTest {
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.requestSpecification = RequestSpec.getRequestSpecification();
+		RestAssured.responseSpecification = ResponseSpec.getSpecOk();
+	}
+
+	@AfterAll
+	static void tearDown() {
+		RestAssured.reset();
+	}
+}
---
+++ src/test/java/com/kit/killrilltesty/tests/BookTest.java
@@ -0,0 +1,104 @@
+package com.kit.killrilltesty.tests;
+
+import com.kit.killrilltesty.api.models.Book;
+import com.kit.killrilltesty.api.specs.ResponseSpec;
+import com.kit.killrilltesty.utils.ModelBuilder;
+import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
+import net.bytebuddy.matcher.StringMatcher;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.util.List;
+
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.not;
+
+public class BookTest extends BaseTest {
+
+	@Test
+	public void testCreateBook() {
+		Book book = ModelBuilder.getBook();
+
+		Assertions.assertEquals(201, book.getId());
+		Assertions.assertEquals("Book 201", book.getTitle());
+
+		Book bookResponse = RestAssured.given()
+				.body(book)
+				.when()
+				.post("/books")
+				.then()
+				.extract()
+				.response()
+				.as(Book.class);
+
+		Assertions.assertEquals(book, bookResponse);
+	}
+
+	@Test
+	public void testGetBookById() throws ParseException {
+		Book book = ModelBuilder.getBookForTestGet();
+
+		Book bookResponse = RestAssured.given()
+				.when()
+				.get("/books/1")
+				.then()
+				.extract()
+				.response()
+				.as(Book.class);
+		Assertions.assertEquals(book, bookResponse);
+		// дату пришлось добавить в exclude для equals, потому что апи возвращает типа LocalDateTime.now(), синхронизировать ответ апи с созданием модели по дате не выйдет(
+	}
+
+	@Test
+	public void testGetAllBooks() {
+		RestAssured.given()
+				.when()
+				.get("/books")
+				.then()
+				.body("", not(empty()));
+
+		List<Book> resp = RestAssured.given()
+				.when()
+				.get("/books")
+				.then()
+				.extract()
+				.response()
+				.as(new TypeRef<>() {});
+
+		Assertions.assertFalse(resp.isEmpty());
+	}
+
+	@Test
+	public void testDeleteBook() {
+		Book book = ModelBuilder.getBook();
+
+		RestAssured.given()
+				.when()
+				.body(book)
+				.post("/books");
+
+		RestAssured.given()
+				.when()
+				.get("/books/201");
+
+		RestAssured.given()
+				.when()
+				.delete("/books/201");
+
+		RestAssured.given()
+				.when()
+				.get("/books/201")
+				.then()
+				.statusCode(404);
+	}
+
+	@Test
+	public void testNegativeGetNonExistingBook() {
+		RestAssured.responseSpecification = ResponseSpec.getSpecNotFound();
+		RestAssured.given()
+				.when()
+				.get("/books/999");
+	}
+}
